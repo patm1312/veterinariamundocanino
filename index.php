0872430cffc70754157e4a54bde9c194ae38ed7c
pedro <tarazonap525@gmail.com>
2023-06-01 16:38:01 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+echo "prueba";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clínica Veterinaria Mundo Canino</title>
+    <link rel="icon" type="image/x-icon" href="assets/images/botoncitas.png">
+</head>
+<body>
+    <header>
+        <div>
+            <img src="assets/images/botoncitas.png" alt="prueba de imagen">
+        </div>
+        <div>
+            <h1>Clínica Veterinaria Mundo Canino</h1>
+        </div>
+        <nav>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Home</h1>
+        <img src="assets/images/botoncitas.png" alt="asignar  cita">
+    </main>
+    <footer>
+        <div>
+            <h2>Enlaces Utiles:</h2>
+            <ul>
+                <ul></ul>
+                <ul></ul>
+                <ul></ul>
+            </ul>
+        </div>
+        <div>
+            <h2>Información de contacto:</h2>
+                <ul>
+                    <ul></ul>
+                    <ul></ul>
+                    <ul></ul>
+                </ul>
+        </div>
+        <div>
+                <h2>Redes sociales:</h2>
+                    <ul>
+                        <ul><img src="assets/images/facebook.svg" alt="social"></ul>
+                        <ul><img src="assets/images/instagram.svg" alt="social"></ul>
+                    </ul>
+        </div>
+    </footer>
+</body>
+</html>
