88eb88315853559ea05fa9848bbd108978184249
pedro <tarazonap525@gmail.com>
2023-06-03 20:37:27 -0500

    primer bosquejo header y footer

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "prueba";
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,52 +8,57 @@ echo "prueba";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clínica Veterinaria Mundo Canino</title>
-    <link rel="icon" type="image/x-icon" href="assets/images/botoncitas.png">
+    <link rel="icon" type="image/x-icon" href="assets/images/faviconlogo.png">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css" />
+    
 </head>
 <body>
-    <header>
-        <div>
-            <img src="assets/images/botoncitas.png" alt="prueba de imagen">
+    <header class="header">
+        <div class="header__boxLogo">
+            <img class="header__boxLogo--imgLogo" src="assets/images/logotransp.png" alt="prueba de imagen">
         </div>
-        <div>
-            <h1>Clínica Veterinaria Mundo Canino</h1>
-        </div>
-        <nav>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <h1>Home</h1>
-        <img src="assets/images/botoncitas.png" alt="asignar  cita">
-    </main>
-    <footer>
-        <div>
-            <h2>Enlaces Utiles:</h2>
-            <ul>
-                <ul></ul>
-                <ul></ul>
-                <ul></ul>
-            </ul>
-        </div>
-        <div>
-            <h2>Información de contacto:</h2>
-                <ul>
-                    <ul></ul>
-                    <ul></ul>
-                    <ul></ul>
+        <div class="header__container">
+            <nav class="nav">
+                <ul class="nav__ul" >
+                    <li class="nav__item">Inicio</li>
+                    <li class="nav__item nav__item--left">Productos/Servicios</li>
+                    <li class="nav__item nav__item--left">Quienes Somos</li>
+                    <li class="nav__item nav__item--login">
+                        <img class="nav__item--loginimg" src="" alt="">
+                    </li>
                 </ul>
+            </nav>
+            <div class="header__boxCita">Agenda Tu cita</div>
+            <div class="header__boxLogin">
+                <p class="header__boxLogin--p"">Registro/Login</p>
+                <img class="header__boxLogin--img" src="assets/images/user.png" alt="">
+            </div>
         </div>
+        </header>
+        <main class="main">
+        <h1>Home</h1>
+        <img class="main__botonCita" src="assets/images/botoncitas.png" alt="asignar  cita">
+    </main>
+    <footer class="footer">
         <div>
-                <h2>Redes sociales:</h2>
-                    <ul>
-                        <ul><img src="assets/images/facebook.svg" alt="social"></ul>
-                        <ul><img src="assets/images/instagram.svg" alt="social"></ul>
-                    </ul>
+            <img src="assets/images/logotransp.png" alt="">
+        </div>
+        <div class="footer__boxenlaces">
+            <h2 class="footer__boxenlaces--h2">Enlaces Utiles:</h2>
+           <a class="footer__boxenlaces--a" href="">Foro</a>
+           <a class="footer__boxenlaces--a" href="">Servicios</a>
+           <a class="footer__boxenlaces--a" href="">Acerca de</a>
+        </div>
+        <div class="footer__boxcontacto">
+            <h2 class="footer__boxcontacto--h2">Información de contacto:</h2>
+                <p class="footer__boxcontacto--p">3153704398</p>
+                <p class="footer__boxcontacto--p">Avenida 3e # 7- 46 Brr. Popular <br> Cucuta, Norte de Santander</p>
+        </div>
+        <div class="footer__social">
+                <h2 class="footer__social--h2">Redes sociales:</h2>
+                        <img class="footer__social--img" src="assets/images/facebook.svg" alt="social">
+                        <img class="footer__social--img" src="assets/images/instagram.svg" alt="social">
+                    
         </div>
     </footer>
 </body>
